7a734da6d296e62814fc588352d1255c4d7d3ddf
Vasu Sharma <vasusharma2017@outlook.com>
2022-09-25 23:58:56 +0530

    Updates

---
+++ precache-manifest.259eae3d7d999124660a366aebbf7782.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "768c07b5ba03f9414d59cc81e6ce7496",
+    "url": "/cse-22-diary/index.html"
+  },
+  {
+    "revision": "c6383dc95ffbfbf4e063",
+    "url": "/cse-22-diary/static/css/main.8a7b6621.chunk.css"
+  },
+  {
+    "revision": "f893a40719d8da2e9566",
+    "url": "/cse-22-diary/static/js/2.ab07c860.chunk.js"
+  },
+  {
+    "revision": "1eb697a7d402f6ed7f3fee095c6290dd",
+    "url": "/cse-22-diary/static/js/2.ab07c860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6383dc95ffbfbf4e063",
+    "url": "/cse-22-diary/static/js/main.7fc92a75.chunk.js"
+  },
+  {
+    "revision": "e436cb5d74f07d22e7bc",
+    "url": "/cse-22-diary/static/js/runtime-main.6b173a0c.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/cse-22-diary/static/media/memories.5c0c63fb.png"
+  }
+]);
